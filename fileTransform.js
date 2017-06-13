eadc18f1092fa9303ff86bb2a8d7a9ed78c2509e
Claire Parker <claire.parker87@gmail.com>
2017-06-13 22:18:13 +0100

    :dvd: Use default param (had to install node 6 locally though!)

--- fileTransform.js
+++ fileTransform.js
@@ -1,22 +1,26 @@
 const path = require("path");
 
-function fileTransform(vendorPath, fileType) {
-    'use strict';
+function fileTransform(vendorPath, fileType = 'js') {
     // Check vendor is valid and exists (does it have to though?)
     let area = "frontend";
     let vendor = "Regatta";
     let theme = "default-desktop";
     // 'vendor/magento/module-wishlist/view/frontend/web/js/wtf.js'
     // app / design / frontend / Regatta / default - desktop / Magento_Wishlist / web / js / wtf.js
+
     // TODO: Find module and construct app version
     let module = "Magento_Wishlist";
     let appPath;
+
     // TODO: Template or xml or JS
     let fileTypePath = "web/js";
+
     // TODO: filename
     let filename = "wtf.js";
-    // TODO: Construct path
+
+    // Construct path
     appPath = path.join("app/design/", area, vendor, theme, module, fileTypePath, filename);
+
     // TODO: Check path doesn't exist already
     return appPath;
 }
--- fileTransform.test.js
+++ fileTransform.test.js
@@ -1,6 +1,6 @@
 const fileTransform = require('./fileTransform');
 
 test('simple happy path', () => {
-  expect(fileTransform('vendor/magento/module-wishlist/view/frontend/web/js/wtf.js', 'js'))
+  expect(fileTransform('vendor/magento/module-wishlist/view/frontend/web/js/wtf.js'))
     .toBe('app/design/frontend/Regatta/default-desktop/Magento_Wishlist/web/js/wtf.js');
 });
