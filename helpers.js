3807342b49931878e059475999bdcce455f6026d
claireparker <claire.parker87@gmail.com>
2017-06-15 13:41:04 +0100

    Add getFileType func

--- helpers.js
+++ helpers.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 let getFileFromPath = function(path) {
+    // Get file type and then you know which bit to ignore up to
     let splitPath = path.split("/");
     return splitPath[splitPath.length - 1];
 }
@@ -28,4 +29,9 @@ let capitalizeFirstLetter = function(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-export {getFileFromPath, getModuleName, transformModuleName};
+let getFileType = function(path) {
+    let splitPath = path.split(".");
+    return splitPath[splitPath.length - 1];
+}
+
+export {getFileFromPath, getModuleName, transformModuleName, getFileType};
--- helpers.test.js
+++ helpers.test.js
@@ -3,8 +3,11 @@ import { getFileFromPath, getModuleName, transformModuleName } from "./helpers";
 // TODO: Few more tests please
 
 test('simple happy path', () => {
-  expect(getFileFromPath('vendor/magento/module-wishlist/view/frontend/web/js/wtf.js'))
-    .toBe('wtf.js');
+    expect(getFileFromPath('vendor/magento/module-wishlist/view/frontend/web/js/wtf.js'))
+        .toBe('wtf.js');
+
+    expect(getFileFromPath('vendor/magento/module-customer-custom-attributes/view/frontend/templates/customer/address/edit.phtml'))
+        .toBe('customer/address/edit.phtml');
 
     expect(getFileFromPath('/abc/def.html'))
         .toBe('def.html');
