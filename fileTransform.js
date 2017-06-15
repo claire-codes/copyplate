f94611e11b43a6183d4772781f0cb625a7f545a7
claireparker <claire.parker87@gmail.com>
2017-06-15 16:29:22 +0100

    WIP

--- fileTransform.js
+++ fileTransform.js
@@ -1,24 +1,27 @@
 const path = require("path");
-import { getFileFromPath, getModuleName } from "./helpers";
+import { getFileFromPath, getModuleName, getFileType } from "./helpers";
 
 // Example:
 // 'vendor/magento/module-wishlist/view/frontend/web/js/wtf.js'
-// app/design/frontend/Regatta/default-desktop/Magento_Wishlist/web/js/wtf.js
-function fileTransform(vendorPath, fileType = 'js') {
+// app/design/frontend/Regatta/default/Magento_Wishlist/web/js/wtf.js
+function fileTransform(vendorPath) {
     // TODO: check absolute or relative
     // Check vendor is valid and exists (does it have to though?)
     let area = "frontend";
     let vendor = "Regatta";
-    let theme = "default-desktop";
+    let theme = "default";
 
     let module = getModuleName(vendorPath);
     let appPath;
 
     // TODO: Template or xml or JS
     // TODO: Find type from extension and remove 2nd param (xml, phtml, html or js or even scss)
-    let fileTypePath;
+    let fileType = getFileType(vendorPath);
+    let fileTypePath = "web/js";
     if (fileType === "js") {
         fileTypePath  = "web/js";
+    } else if (fileType === "phtml") {
+        fileTypePath = "templates";
     }
 
     let filename = getFileFromPath(vendorPath);
--- fileTransform.test.js
+++ fileTransform.test.js
@@ -1,6 +1,9 @@
 const fileTransform = require('./fileTransform');
 
-test('simple happy path', () => {
+test('Global JS in app/design', () => {
   expect(fileTransform('vendor/magento/module-wishlist/view/frontend/web/js/wtf.js'))
-    .toBe('app/design/frontend/Regatta/default-desktop/Magento_Wishlist/web/js/wtf.js');
+    .toBe('app/design/frontend/Regatta/default/Magento_Wishlist/web/js/wtf.js');
+
+expect(fileTransform('vendor/magento/module-customer-custom-attributes/view/frontend/templates/customer/address/edit.phtml', 'phtml'))
+    .toBe('app/design/frontend/Regatta/default/Magento_CustomerCustomAttributes/templates/customer/address/edit.phtml');
 });
