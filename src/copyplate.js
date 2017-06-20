9860ca877951e67d6ee28521f19fb45f1c91bdd6
Claire Parker <claire.parker87@gmail.com>
2017-06-20 22:25:11 +0100

    :pig: Check whether file already exists before creating it

--- src/copyplate.js
+++ src/copyplate.js
@@ -2,24 +2,28 @@ const path = require("path");
 const fs = require("fs");
 const fileTransform = require('./fileTransform');
 
-// TODO: Get arg
 var args = process.argv.slice(2);
 var vendorPath = args[0];
 var appPath;
 
-// TODO: Decide whether to make file (are args OK, is fileTransform result OK?)
 if (vendorPath) {
-    appPath = fileTransform(vendorPath);
+    if (fs.existsSync(vendorPath))  {
+        appPath = fileTransform(vendorPath);
+    } else {
+        console.log("File does not exist");
+        process.exit();
+    }
 } else {
     console.log("You haven't provided me with an argument");
     process.exit();
 }
-// TODO: Does current one exist - copy content
+
+// TODO: Copy content rather than create blank file
 var filePath = 'foo.txt';
 
-// TODO: Check whether file exists or not already
 if (fs.existsSync(filePath)) {
     console.log("It already exists");
+    process.exit();
 } else {
     console.log("It doesn't exist yet, I'll make it");
     fs.closeSync(fs.openSync(filePath, 'w'));
